48d4cf02cefd7fc5569c3d2d4185953311b02ce7
William Zhou <aznzhang@gmail.com>
2015-08-06 16:34:14 -0400

    Terrain done, except for snowballs

--- Assets/scripts/worldControl.cs
+++ Assets/scripts/worldControl.cs
@@ -5,42 +5,75 @@ using UnityEngine.UI;
 public class worldControl : MonoBehaviour {
 	public GameObject box;
 	private Rigidbody boxPhys;
+	public float pushForce;
+	private int pushDelayCount = 0;
+	private bool tilted = false;
+	private Vector3 snowballDest;
+	public int pushDelayTime = 200;
 	public float tiltForce;
 	public float returnForce;
 	
 	void Start () {
 		boxPhys = box.GetComponent<Rigidbody>();
 	}
-	void Update () {
-		Debug.Log (transform.eulerAngles.x);
+
+	void Update(){
+		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+		RaycastHit rayHit = new RaycastHit();
+		if(Physics.Raycast (ray, out rayHit, 1000f) && Input.GetMouseButtonDown(0)){
+			snowballDest = rayHit.point;
+			//check x or z position, if above a certain number, Destroy
+		}
 	}
 
 	void FixedUpdate () {
-		if (transform.eulerAngles.x < 1f || transform.eulerAngles.x > 359f){
-			if (Input.GetKeyDown (KeyCode.W)){
-				boxPhys.AddTorque (tiltForce,0f,0f);
+		//terrain shift
+		if(pushDelayCount > 0)
+			pushDelayCount++;
+		if(pushDelayCount == pushDelayTime)
+			pushDelayCount = 0;
+		if (Input.GetKeyDown(KeyCode.Mouse0)){
+			if(pushDelayCount == 0){
+				boxPhys.AddForce(-pushForce,0f,0f);
+				pushDelayCount++;
 			}
-			if(Input.GetKeyDown (KeyCode.S)){
-				boxPhys.AddTorque (-tiltForce,0f,0f);
+		}	
+
+		//tilt if near-flat
+		if (transform.eulerAngles.x < .1f || transform.eulerAngles.x > 359.9f){
+			if (tilted){
+				transform.rotation = Quaternion.identity;
+				boxPhys.angularVelocity = Vector3.zero;
+				tilted = false;
+			}
+			if (Input.GetAxis ("Mouse ScrollWheel") > 0){
+				boxPhys.AddTorque (tiltForce,0f,0f,ForceMode.VelocityChange);
+			}
+			if(Input.GetAxis ("Mouse ScrollWheel") < 0){
+				boxPhys.AddTorque (-tiltForce,0f,0f,ForceMode.VelocityChange);
 			}
 		}
 
-		//CLAMPING
+		//clamp tilt
 		float xRotation = transform.eulerAngles.x;
 		bool tiltedRight = false;
 		if (xRotation > 180) {
 			tiltedRight = true;
 			xRotation = 360 - xRotation;
 		}
-		xRotation = Mathf.Clamp(xRotation, 0, 10);
+		xRotation = Mathf.Clamp(xRotation, 0f, 10f);
 		if (tiltedRight) 
 			xRotation = 360 - xRotation;
 		transform.rotation = Quaternion.Euler (new Vector3(xRotation,0f,0f)); 
 
 		//return tilt to 0
-		if (transform.eulerAngles.x > 1f && transform.eulerAngles.x < 180f)
+		if (transform.eulerAngles.x > .1f && transform.eulerAngles.x < 180f){
 			boxPhys.AddTorque (-returnForce,0f,0f);
-		if(transform.eulerAngles.x < 359f && transform.eulerAngles.x > 180f)
+			tilted = true;
+		}
+		if(transform.eulerAngles.x < 359.9f && transform.eulerAngles.x > 180f){
 			boxPhys.AddTorque (returnForce,0f,0f);
+			tilted = true;
+		}
 	}
 }
