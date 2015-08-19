935897502974aa1466a752ef01e9d9590f5c29c5
Jun Ma <seven2277s@gmail.com>
2015-08-18 20:05:12 -0400

    Add audios. Changed the ice ground color. Add skiing particle.

--- Assets/scripts/VehicleControl.cs
+++ Assets/scripts/VehicleControl.cs
@@ -8,6 +8,9 @@ public class VehicleControl : MonoBehaviour {
 	public float moveStrength = 300;
 	public bool twoCharacters = false;
 
+	public ParticleSystem icePar1;
+	public ParticleSystem icePar2;
+	public AudioSource icing;
 
 	// Use this for initialization
 	void Start () {
@@ -19,36 +22,46 @@ public class VehicleControl : MonoBehaviour {
 		if (Input.GetKeyDown(KeyCode.W))
 		{
 			p1body.AddForce(transform.forward * moveStrength);
+			icing.Play();
 		}
 		if (Input.GetKeyDown(KeyCode.S))
 		{
 			p1body.AddForce(-transform.forward * moveStrength);
+			icing.Play();
 		}
 		if (Input.GetKeyDown(KeyCode.A))
 		{
 			p1body.AddForce(-transform.right * moveStrength);
+			icing.Play();
 		}
 		if (Input.GetKeyDown(KeyCode.D))
 		{
 			p1body.AddForce(transform.right * moveStrength);
+			icing.Play();
+			icePar1.Play();
 		}
 		if (twoCharacters)
 		{
 			if (Input.GetKeyDown(KeyCode.I))
 			{
 				p2body.AddForce(transform.forward * moveStrength);
+				icing.Play();
 			}
 			if (Input.GetKeyDown(KeyCode.K))
 			{
 				p2body.AddForce(-transform.forward * moveStrength);
+				icing.Play();
 			}
 			if (Input.GetKeyDown(KeyCode.J))
 			{
 				p2body.AddForce(-transform.right * moveStrength);
+				icing.Play();
 			}
 			if (Input.GetKeyDown(KeyCode.L))
 			{
 				p2body.AddForce(transform.right * moveStrength);
+				icing.Play();
+				icePar2.Play();
 			}
 		}
 	}
